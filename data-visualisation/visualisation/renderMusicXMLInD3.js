dd67869aa759549004a25ebdb6c88e02e4886430
Jamie Gabriel <jamiegabriel@Jamies-MacBook-Pro.local>
2016-08-03 20:19:45 +1000

    fix location starts

--- data-visualisation/visualisation/renderMusicXMLInD3.js
+++ data-visualisation/visualisation/renderMusicXMLInD3.js
@@ -33,7 +33,49 @@ var tooltip = d3.select("body")
     .style("background", "steelblue")
     .style("color", "white")
     .style("border-radius", "5px")
-    .style("padding", "3px");
+    .style("padding", "3px")
+    .style("font-size", "10px");
+
+
+function createTextForToolTip(absLocation,
+                               beatType,
+                               beats,
+                               currentVoice,
+                               duration,
+                               durationWithNotations,
+                               instrument,
+                               isHarmony,
+                               location,
+                               measure,
+                               measureLocationInQuarterNotes,
+                               midiNumber) {
+    return "<strong>Location:</strong> " 
+        + absLocation
+        + "<br><strong>Time signature:</strong> " 
+        + beatType + '/' + beats
+        + "<br><strong>Current voice:</strong> " 
+        + currentVoice
+        + "<br><strong>Duration:</strong> " 
+        + duration 
+        + "<br><strong>Duration with notations:</strong> " 
+        + durationWithNotations
+        + "<br><strong>Instrument:</strong> " 
+        + instrument
+        + "<br><strong>Is harmony:</strong> " 
+        + isHarmony
+        + "<br><strong>Location in measure:</strong> " 
+        + location
+        + "<br><strong>Measure:</strong> " 
+        + measure
+        + "<br><strong>Measure location in quarter notes:</strong> " 
+        + measureLocationInQuarterNotes
+        + "<br><strong>Midi number</strong> " 
+        + midiNumber
+      
+
+
+}
+
 
 
 
@@ -52,18 +94,33 @@ d3.json("data/rawData.json", function (error, data) {
                 })
                 .attr("x", function (d, i) {
 
-                    return d.absLocation / 256 * 25;
+                    return (d.absLocation / 256 * 25) + 50;
                 })
                 .attr("y", function(d,i) {
                     
                     return 880 - (d.midiNumber * 10);
                 })
-                .on("mouseover", function(d){
-                    var el = d3.select(this).attr("width")
+                .attr("fill", "gray")
+                .on("mouseover", function(d, i){
                     
-                    console.log(el);
-                    var textForTip = "<strong>Pitch:</strong> " + el + ", Midi number " + d.midiNumber;
-                    tooltip.html(textForTip); 
+                    var dataForNode = d3.select(this).datum();
+                    console.log(dataForNode);
+                    var duration = d3.select(this).attr("width")
+                    var pitch = d3.select(this).attr("y")
+                    var location = d3.select(this).attr("x")
+                    var textForDisplay = createTextForToolTip(dataForNode.absLocation, 
+                                                              dataForNode.beatType, 
+                                                              dataForNode.beats, 
+                                                              dataForNode.currentVoice, 
+                                                              dataForNode.duration,
+                                                              dataForNode.durationWithNotations, 
+                                                              dataForNode.instrument, 
+                                                              dataForNode.isHarmony,
+                                                              dataForNode.location, 
+                                                              dataForNode.measure, 
+                                                              dataForNode.measureLocationInQuarterNotes, 
+                                                              dataForNode.midiNumber)
+                    tooltip.html(textForDisplay); 
                     return tooltip.style("visibility", "visible");
                 })
                 .on("mousemove", function(){
--- musicXML2JSON/parsingMethods/addAttributes.js
+++ musicXML2JSON/parsingMethods/addAttributes.js
@@ -1,7 +1,9 @@
 module.exports.addAttributes = function (musicData) {
     var arrayToHoldEachInstrumentSeperately = musicData;
     var measureStartingLocationInQuarterNotes = 0;
+    
     for (var i = 0; i < arrayToHoldEachInstrumentSeperately.length; i++) {
+        var measureStartingLocationInQuarterNotes = 0;
         for (var j = 0; j < arrayToHoldEachInstrumentSeperately[i].length; j++) {
             if (arrayToHoldEachInstrumentSeperately[i][j].attributes) {
                 if (arrayToHoldEachInstrumentSeperately[i][j].attributes[0].time) {
