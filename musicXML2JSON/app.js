a63846ff24059d486f708c55fdd2adff6102ad8e
Jamie Gabriel <jamiegabriel@Jamies-MacBook-Pro.local>
2016-08-07 10:48:35 +1000

    update arg passingwith minimist

--- musicXML2JSON/app.js
+++
@@ -1,8 +0,0 @@
-
-var parseMusicXML = require('./parseMusicXML')
-
-parseMusicXML.parseRawMusicXML('/corpus/test3.xml');
-
-
-
-
---
+++ musicXML2JSON/convertMusicXML2MusicJSON.js
@@ -0,0 +1,17 @@
+
+var parseMusicXML = require('./parseMusicXML')
+var argv = require('minimist')(process.argv.slice(2));
+
+
+if (argv["f"]) {
+    console.log('Converting' + argv["f"] + ' to musicJSON...')
+    parseMusicXML.parseRawMusicXML(argv["f"]);
+}
+else {
+    console.log('You need to pass a musicXML file using "-f pathToYourFile.xml". \nIf you don\'t have a musicXML file handy there are some samples in the ./corpus folder');
+}
+
+
+
+
+
